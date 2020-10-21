7dd2c07da94995dbc2fd56ce87cd6ec574bb2b7b
Kamil Przeniosło <kamil.przenioslo@gazeta.pl>
2020-10-21 21:11:10 +0200

    Add finished game dialog, improve gaming experience

--- app/src/main/java/pl/mazurprzenioslo/tilegame/GameActivity.kt
+++ app/src/main/java/pl/mazurprzenioslo/tilegame/GameActivity.kt
@@ -1,26 +1,33 @@
 package pl.mazurprzenioslo.tilegame
 
 import Tile
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.widget.ArrayAdapter
 import android.widget.GridView
 import android.widget.TextView
-import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
 import androidx.core.view.children
+import androidx.fragment.app.DialogFragment
+import androidx.fragment.app.FragmentActivity
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers.Main
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 import pl.mazurprzenioslo.tilegame.databinding.ActivityGameBinding
+import java.util.*
 import java.util.concurrent.atomic.AtomicInteger
-import kotlin.concurrent.thread
+import kotlin.concurrent.timerTask
 
 
-class GameActivity : AppCompatActivity() {
+class GameActivity : FragmentActivity(), GameFinishedDialogFragment.GameFinishedDialogListener {
     private lateinit var binding: ActivityGameBinding
     private val tiles = Array(GRID_COLUMNS * GRID_ROWS) { Tile() }
     private lateinit var tilesGridView: GridView
     private lateinit var counterTextView: TextView
-    var filledBoxesCount = AtomicInteger()
-    var clearedBoxesCounter = AtomicInteger()
+    private var filledBoxesCount = AtomicInteger()
+    private var clearedBoxesCounter = AtomicInteger()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -44,10 +51,14 @@ class GameActivity : AppCompatActivity() {
             Log.d("Log", "Height: " + tilesGridView.height)
             tilesGridView.children.iterator()
                 .forEach { tile ->
-                    tile.layoutParams.height = tilesGridView.height / GRID_ROWS - 6
+                    tile.layoutParams.height = tilesGridView.height / GRID_ROWS - 4
                 }
 
-            startGame()
+            CoroutineScope(Main).launch {
+                startGame()
+            }.invokeOnCompletion {
+                showRoundNumbersDialog()
+            }
         }
     }
 
@@ -59,29 +70,23 @@ class GameActivity : AppCompatActivity() {
         counterTextView.text = clearedBoxesCounter.incrementAndGet().toString()
     }
 
-    private fun startGame(): Thread {
+    private suspend fun startGame() {
         var gameFinished = false
+        var timeToFillMs = INITIAL_TIME_TO_FILL_MS
 
-        return thread(start = true) {
-            var timeToFillMs = INITIAL_TIME_TO_FILL_MS
+        resetGame()
+        //TODO countdown 3 2 1
+        delay(1000)
+        Timer().schedule(timerTask { timeToFillMs -= 17 }, 7000, 4000)
 
-            while (!gameFinished) {
-                if (filledBoxesCount.get() == tiles.size) {
-                    gameFinished = true
-//                    val intent = Intent()
-//                    intent.putExtra(CLEARED_BOXES_KEY, clearedBoxesCounter)
-//                    setResult(CLEARED_BOXES_RESULT_OK, intent)
-//                    finish()
-                    return@thread
-                } else {
-                    fillRandomTile()
-                }
-
-                if (clearedBoxesCounter.get() % 9 == 0) {
-                    timeToFillMs -= 15
-                }
-                Thread.sleep(timeToFillMs)
+        while (!gameFinished) {
+            if (filledBoxesCount.get() == tiles.size) {
+                gameFinished = true
+            } else {
+                fillRandomTile()
             }
+
+            delay(timeToFillMs)
         }
     }
 
@@ -96,12 +101,40 @@ class GameActivity : AppCompatActivity() {
         }
     }
 
+    private fun showRoundNumbersDialog() {
+        GameFinishedDialogFragment().apply { isCancelable = false }
+            .show(supportFragmentManager, "gameFinishedDialog")
+    }
+
+    private fun resetGame() {
+        tilesGridView.children.iterator()
+            .forEach { tile -> tile.setBackgroundColor(getColor(R.color.white)) }
+        tiles.forEach { tile -> tile.filled = false }
+        filledBoxesCount.set(0)
+        clearedBoxesCounter.set(0)
+        counterTextView.text = "0"
+    }
+
     companion object {
         const val GRID_COLUMNS = 4
         const val GRID_ROWS = 7
         const val INITIAL_TIME_TO_FILL_MS = 500L;
-        const val CLEARED_BOXES_REQUEST_CODE = 666
-        const val CLEARED_BOXES_RESULT_OK = 4321
-        const val CLEARED_BOXES_KEY = "clearedBoxes"
+        const val FINISHED_PLAYING_RESULT_OK = 4321
+        const val FINISHED_PLAYING_KEY = "finishedPlaying"
+    }
+
+    override fun onReturnToMainMenu(dialog: DialogFragment) {
+        val intent = Intent()
+        intent.putExtra(FINISHED_PLAYING_KEY, clearedBoxesCounter)
+        setResult(FINISHED_PLAYING_RESULT_OK, intent)
+        finish()
+    }
+
+    override fun onPlayAgain(dialog: DialogFragment) {
+        CoroutineScope(Main).launch {
+            startGame()
+        }.invokeOnCompletion {
+            showRoundNumbersDialog()
+        }
     }
 }
---
+++ app/src/main/java/pl/mazurprzenioslo/tilegame/GameFinishedDialogFragment.kt
@@ -0,0 +1,42 @@
+package pl.mazurprzenioslo.tilegame
+
+import android.app.Dialog
+import android.content.Context
+import android.os.Bundle
+import androidx.appcompat.app.AlertDialog
+import androidx.fragment.app.DialogFragment
+
+class GameFinishedDialogFragment : DialogFragment() {
+    private lateinit var listener: GameFinishedDialogListener
+
+    interface GameFinishedDialogListener {
+        fun onReturnToMainMenu(dialog: DialogFragment)
+        fun onPlayAgain(dialog: DialogFragment)
+    }
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        try {
+            listener = context as GameFinishedDialogListener
+        } catch (e: ClassCastException) {
+            throw ClassCastException(("$context must implement GameFinishedDialogListener"))
+        }
+    }
+
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        return activity?.let {
+            val array =
+                arrayOf(getString(R.string.return_to_main_menu), getString(R.string.play_again))
+            val builder = AlertDialog.Builder(it)
+            builder.setTitle(getString(R.string.game_finished))
+                .setCancelable(false)
+                .setItems(array) { _, checkedItemIndex ->
+                    when (checkedItemIndex) {
+                        0 -> listener.onReturnToMainMenu(this)
+                        1 -> listener.onPlayAgain(this)
+                    }
+                }
+            builder.create()
+        } ?: throw IllegalStateException("Activity cannot be null")
+    }
+}
